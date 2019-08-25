eb8d5d2bd553b02585c8d4f73167b6d04c6d2ab9
stasbovino <stasbovino@mail.ru>
2019-08-25 17:30:26 +0300

    upd

---
+++ push_swap.h
@@ -0,0 +1,13 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "libft.h"
+#include <stdlib.h>
+
+typedef struct	s_stack
+{
+	int				n;
+	struct s_stack	*next;
+}				t_stack;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,100 @@
+#include "push_swap.h"
+
+int		check_isnum(char *num)
+{
+	int i;
+
+	i = (num[0] == '-') ? 0 : -1;
+	while (num[++i])
+		if (!(num[i] >= '0' && num[i] <= '9'))
+			return (1);
+	return (0);
+}
+
+int		check_overflow(char *num)
+{
+	int		i;
+	char	*overf;
+	int		lenoverf;
+
+	overf = (num[0] == '-') ? "-2147483648" : "2147483647";
+	lenoverf = (num[0] == '-') ? 11 : 10;
+	if (ft_strlen(num) > lenoverf)
+		return (1);
+	else if (ft_strlen(num) < lenoverf)
+		return (0);
+	i = (num[0] == '-') ? 0 : -1;
+	while (num[++i])
+	{
+		if (num[i] > overf[i])
+			return (1);
+	}
+	return (0);
+}
+
+t_stack	*create_struct(void)
+{
+	t_stack *a;
+
+	a = (t_stack*)malloc(sizeof(t_stack));
+	a->n = 0;
+	a->next = NULL;
+	return (a);
+}
+
+t_stack	*create_stack(int argc, char **argv)
+{
+	t_stack *prev;
+	t_stack	*new;
+	int		i;
+	t_stack	*begin;
+
+	new = create_struct();
+	begin = new;
+	new->n = ft_atoi(argv[1]);
+	prev = new;
+	i = 1;
+	while (++i < argc)
+	{
+		new = create_struct();
+		new->n = ft_atoi(argv[i]);
+		prev->next = new;
+		prev = new;
+	}
+	return (begin);
+}
+
+void	print_stack(t_stack *a)
+{
+	while (a)
+	{
+		ft_printf("%d\n", a->n);
+		a = a->next;
+	}
+}
+
+int		error(void)
+{
+	write(2, "Error\n", 6);
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack *a;
+	int		i;
+
+	if (argc <= 1)
+		return (error());
+	i = 0;
+	while (++i < argc)
+	{
+		if (check_isnum(argv[i]))
+			return (error());
+		if (check_overflow(argv[i]))
+			return (error());
+	}
+	a = create_stack(argc, argv);
+	print_stack(a);
+	return (0);
+}
