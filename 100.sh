d220876f222f8c493247c14d3879c1ae7f679d8a
stasbovino <stasbovino@mail.ru>
2019-09-01 15:35:10 +0300

    .sh files works on mac

--- 100.sh
+++ 100.sh
@@ -2,8 +2,9 @@ for ((i=99; i>=0; i--));
 do 
 	if (( $i == 0 ))
 	then
-		echo -n $i;
+		printf $i;
 	else
-		echo -n "$i ";
+		printf "$i ";
 	fi
 done > b.txt
+ARG=`cat b.txt`; ./push_swap $ARG | wc -l
